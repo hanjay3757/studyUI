d1bdfc4033702ff936d4d7f68311ae0c65dbdd27
hanjay3757 <hanjay3757@gmail.com>
2025-05-26 11:26:28 +0900

    수업업

--- react/react_test/src/App.js
+++ react/react_test/src/App.js
@@ -1,13 +1,15 @@
+import React from "react";
 import "./App.css";
 // import UseMemo37 from "./components/UseMemo37";
-import UseMemo38 from "./components/UseMemo38";
-import React from "react";
+// import UseMemo38 from "./components/UseMemo38";
+import UseCallback39 from "./components/UseCallback39";
 
 function App() {
   return (
     <div className="App">
       {/* <UseMemo37 /> */}
-      <UseMemo38 />
+      {/* <UseMemo38 /> */}
+      <UseCallback39 />
     </div>
   );
 }
