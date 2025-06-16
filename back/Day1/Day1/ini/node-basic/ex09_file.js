01155ad2dbb65a39a9209ce7fac5d8b77e314a8a
한재우 <hanjay3757@gmail.com>
2025-06-16 13:54:58 +0900

    nodejs

--- back/Day1/Day1/ini/node-basic/ex09_file.js
+++ back/Day1/Day1/ini/node-basic/ex09_file.js
@@ -1,6 +1,20 @@
 const fs = require("fs");
 
-// 동기 방식
+// 동기 방식 readFilesync
 const data = fs.readFileSync("package.json", "utf8");
+console.log("동기");
 console.log(data);
 console.log("seeya");
+// 비동기 방식으로 읽기 readFile(파일명 인코딩 콜백함수)
+console.log(
+  "---------------------비동기 방식으로 파일 읽기 시작-------------------"
+);
+fs.readFile("ex01_global2.js", "utf8", function (err, data) {
+  // callback 함수
+  if (err) throw err; //console.error(err)
+  console.log("비동기");
+  console.log(data);
+  console.log("종료");
+  return;
+});
+
